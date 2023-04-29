b81564730c89a39715712d5710cf47651a07ad63
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-29 23:35:23 +0600

    I created a constructor for the ArrayListQueue class. This constructor initializes an instance variable queue of type ArrayList<T> as an empty ArrayList.

--- ListQueue.java
+++ ListQueue.java
@@ -4,5 +4,9 @@ import java.util.NoSuchElementException;
 public class ArrayListQueue<T> {
     private ArrayList<T> queue;
 
+    public ArrayListQueue() {
+        queue = new ArrayList<>();
+    }
+
 
 }
