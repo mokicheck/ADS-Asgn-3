042002ba96d13a8626c5fad46cf6954dbcf87d64
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-04-29 23:48:07 +0600

    Here is documentation of class ListQueue.java

--- ListQueue.java
+++ ListQueue.java
@@ -1,30 +1,64 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
-public class ArrayListQueue<T> {
+/*
+  This class implements a queue using an ArrayList.
+  @<T> the type of the elements in the queue
+ */
+public class ListQueue<T> {
     private ArrayList<T> queue;
 
-    public ArrayListQueue() {
+    //Creates an empty queue.
+
+    public ListQueue() {
         queue = new ArrayList<>();
     }
 
-    public void enqueue(T data) // This function add data to the back of the queue
-    {
+    //Adds an element to the back of the queue.
+    //@data the element to add
+
+    public void enqueue(T data) {
         queue.add(data);
     }
 
-    public boolean isEmpty() // This function checks, is queue empty and returns true or false
-    {
+    /*
+     * Checks if the queue is empty.
+
+     * @return true if the queue is empty, false otherwise
+     */
+    public boolean isEmpty() {
         return queue.isEmpty();
     }
-    public T dequeue () // This function removes and returns the front element of the queue.
-    {
-        if (isEmpty())
-        {
+
+    /*
+      Removes and returns the front element of the queue.
+      @return the front element of the queue
+      @throws NoSuchElementException if the queue is empty
+     */
+    public T dequeue() {
+        if (isEmpty()) {
             throw new NoSuchElementException();
         }
         return queue.remove(0);
     }
 
+    /*
+      Returns the front element of the queue without removing it.
+      @return the front element of the queue
+      @throws NoSuchElementException if the queue is empty
+     */
+    public T peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        return queue.get(0);
+    }
 
+    /*
+
+      @return the number of elements in the queue
+     */
+    public int getSize() {
+        return queue.size();
+    }
 }
